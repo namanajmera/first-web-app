703bf41630ce0958afe4bbb75563948cc208f963
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-07-29 18:44:08 +0530

    Added: Login and use of get and post method and redirect to next page.

--- src/main/java/com/naman/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/naman/springboot/myfirstwebapp/login/LoginController.java
@@ -3,6 +3,8 @@ package com.naman.springboot.myfirstwebapp.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.ModelMap;
@@ -12,10 +14,15 @@ public class LoginController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/login")
-    public String goToLoginPage(@RequestParam String name, ModelMap modelMap){
-        modelMap.put("name",name);
-        logger.info("Request param is:- "+ name);
+    @GetMapping("/login")
+    public String goToLoginPage(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap modelMap){
+        modelMap.put("name",name);
+        modelMap.put("password",password);
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Welcome Page</title>
+    </head>
+    <body>
+        Welcome to the Naman!
+        <div>
+            <p>
+                Your Name:- ${name}
+            </p>
+            <p>
+                Your Password: ${password}
+            </p>
+        </div>
+    </body>
+</html>
