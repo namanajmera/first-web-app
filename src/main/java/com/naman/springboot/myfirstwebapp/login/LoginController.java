d280c0dc5be061992eeb3f768c479828ab39db9e
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-07-29 18:19:29 +0530

    Added: Add the logger.

--- src/main/java/com/naman/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/naman/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.naman.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +10,12 @@ import org.springframework.ui.ModelMap;
 @Controller
 public class LoginController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/login")
     public String goToLoginPage(@RequestParam String name, ModelMap modelMap){
         modelMap.put("name",name);
-        System.out.println("Request param is:- "+ name);
+        logger.info("Request param is:- "+ name);
         return "login";
     }
 }
