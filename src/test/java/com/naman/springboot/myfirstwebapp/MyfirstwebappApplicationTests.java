b4515ff32d8f6f9112cc973b479055f42cf63612
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-07-31 19:05:03 +0530

    Added: JUnit Testing.

--- src/test/java/com/naman/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
+++ src/test/java/com/naman/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -3,11 +3,44 @@ package com.naman.springboot.myfirstwebapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class MyfirstwebappApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    List<String> todos = Arrays.asList("AWS", "Azure", "DevOps");
+
+    @Test
+    void testAsserts() {
+        boolean test = todos.contains("AWS");//Result
+        boolean test2 = todos.contains("GCP");//Result
+        assertTrue(test);
+        assertFalse(test2);
+        assertEquals(3, todos.size());
+    }
+
+    private MyMath math = new MyMath();
+
+    @Test
+    void calculateSum_ThreeMemberArray() {
+        assertEquals(6, math.calculateSum(new int[] {1,2,3}));
+    }
+
+    @Test
+    void calculateSum_ZeroLengthArray() {
+        assertEquals(0, math.calculateSum(new int[] {}));
+    }
+
+    @Test
+    void calculateSum_RerunFalse() {
+        assertNotEquals(16, math.calculateSum(new int[] {1,2,3,4,5}));
     }
 
 }
